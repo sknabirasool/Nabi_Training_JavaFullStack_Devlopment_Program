ee491379ec02847160aca9f1b588a86f2dc55259
sknabirasool <sknabirasool104@gmail.com>
2023-08-14 22:30:24 +0530

    file added

--- Nabi_Training_JavaFullStack_Devlopment_Program/src/main/java/org/java/example/day0/Demo.java
+++ Nabi_Training_JavaFullStack_Devlopment_Program/src/main/java/org/java/example/day0/Demo.java
@@ -1,7 +1,27 @@
 package org.java.example.day0;
 
 public class Demo {
+    // Method to validate age for voting
+    public void validateAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("User is not eligible for voting");
+        } else {
+            System.out.println("User is eligible for voting");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello New Java Demo");
+        Demo validator = new Demo();
+
+        int[] ages = {16, 20, 30, 15}; // Sample ages
+
+        for (int age : ages) {
+            try {
+                System.out.println("Checking eligibility for age: " + age);
+                validator.validateAge(age);
+            } catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
